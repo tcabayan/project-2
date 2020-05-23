bbfe9160870ae7b1f47f7b8532860ecf6cee0d67
KAT <katreadwell1@gmail.com>
2020-05-23 13:46:41 -0700

    commented out stuff casuing issues

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const exphbs = require('express-handlebars');
 const morgan = require('morgan');
-const passport = require('./config/passport');
+// const passport = require('./config/passport');
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,13 +12,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static('public'));
 // need session to keep track of user's login status
-app.use(session({ secret: '???', resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(session({ secret: '???', resave: true, saveUninitialized: true }));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 // require routes
-require('./routes/api-route-login')(app);
-require('./routes/html-route-login')(app);
+// require('./routes/api-route-login')(app);
+// require('./routes/html-route-login')(app);
 
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
