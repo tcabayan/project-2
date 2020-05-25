c7fb71335f95601c34e88ea4da039761f23fa22d
Tamar Cabayan <tcabayan@gmail.com>
2020-05-25 10:17:24 -0700

    updated webpack files

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,8 +1,49 @@
+const path = require ("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require ("clean-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    devtool: "none",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.[contentHash].js",
+        path: path.resolve(_dirname, "dist")
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: "./views/layouts/main.handlebars"
+    })],
+    plugins: [new CleanWebpackPlugin()], 
+    module: {
+        rules: [
+           {
+               test: /\.scss$/,
+               use: ["style-loader", "css-loader", "sass-loader"]
+           }, 
+           {
+               test: /\.handlebars$/,
+               use: ["html-loader"]
+           },
+           {
+            test: /\.(svg|png|jpg|gif)$/,
+            use: {
+                loader: "file-loader",
+                options: {
+                    name: "[name].[hash].[ext]",
+                    outputPath: "imgs",
+                }
+            }
+            }
+        ] 
+    }
+};
+
+
+
+
 //@ts-check
 
 'use strict';
-
-const path = require('path');
 
 /**@type {import('webpack').Configuration}*/
 const config = {
