53139165ce1c30951174f508e1efb98c6d621374
Christopher Zenner <christopher.zenner@gmail.com>
2020-05-26 20:13:40 -0700

    fix: address linting errors

--- routes/html.js
+++ routes/html.js
@@ -1,4 +1,3 @@
-const db = require('../models');
 const router = require('express').Router();
 
 const Parser = require('rss-parser');
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV !== 'production') {
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-app.use(require('./routes/html'))
+app.use(require('./routes/html'));
 app.use('/api', require('./routes/api'));
 
 app.listen(PORT, () => console.log('Visit https://localhost:3000 in your broswer.'));
