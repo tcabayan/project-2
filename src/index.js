d63565447a34077a57e4840d927fe902c1ef45fd
Christopher Zenner <christopher.zenner@gmail.com>
2020-05-26 20:10:54 -0700

    chore: clean up codebase

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import 'bootstrap';
-import 'bootstrap/js/dist/util';
-import 'bootstrap/js/dist/dropdown';
-import './main.scss';
+require('bootstrap');
+// require('bootstrap/js/dist/util');
+// require('bootstrap/js/dist/dropdown');
+require('main.scss');
