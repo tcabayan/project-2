65fc53897982629197665e649477fd7a088fe291
KAT <katreadwell1@gmail.com>
2020-05-26 22:30:32 -0700

    passport worksgit add -Agit add -A!

--- routes/passport.js
+++ routes/passport.js
@@ -1,39 +1,17 @@
-const express = require('express');
-const app = express();
 const passport = require('passport');
-
-// user not logged in
-app.get('/', function (req, res) {
-  res.render('index', { user: req.user });
-});
-
-// user logged in
-app.get('/', ensureAuthenticated, function (req, res) {
-  res.render('index', { user: req.user });
-});
+const router = require('express').Router();
 
 // send out github auth get request
-app.get('/auth/github',
+router.get('/github',
   passport.authenticate('github', { scope: ['user:email'] }),
   function (req, res) {
   });
 
 // callback for github auth
-app.get('/auth/github/callback',
-  passport.authenticate('github', { failureRedirect: '/login' }),
+router.get('/github/callback',
+  passport.authenticate('github', { failureRedirect: '/' }),
   function (req, res) {
     res.redirect('/');
   });
 
-// logout - end session
-app.get('/logout', function (req, res) {
-  req.logout();
-  res.redirect('/');
-});
-
-app.listen(3000);
-
-function ensureAuthenticated (req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/');
-}
+module.exports = router;
--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const exphbs = require('express-handlebars');
 const morgan = require('morgan');
 const passport = require('passport');
 const session = require('express-session');
-const methodOverride = require('method-override');
 const GitHubStrategy = require('passport-github2').Strategy;
-const partials = require('express-partials');
 const PORT = process.env.PORT || 3000;
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
@@ -17,6 +15,7 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static('public'));
+app.use(session({ secret: process.env.COOKIE_SECRET, resave: false, saveUninitialized: false }));
 
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
@@ -45,12 +44,11 @@ function (accessToken, refreshToken, profile, done) {
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.use(require('./routes/html'));
 app.use('/api', require('./routes/api'));
-app.use(require('./routes/passport'));
+app.use('/auth', require('./routes/passport'));
 
-app.listen(PORT, () => console.log('Visit https://localhost:3000 in your broswer.'));
+app.listen(PORT, () => console.log('Visit http://localhost:3000 in your broswer.'));
