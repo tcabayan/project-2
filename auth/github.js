3c4c0d9eb80b5a7186e34d41f8f4370d740aa144
KAT <katreadwell1@gmail.com>
2020-05-29 20:03:45 -0700

    readme updated, needs styling once pushed up

--- auth/github.js
+++ auth/github.js
@@ -9,7 +9,8 @@ const CLIENT_SECRET = process.env.CLIENT_SECRET;
 passport.use(new GitHubStrategy({
   clientID: CLIENT_ID,
   clientSecret: CLIENT_SECRET,
-  callbackURL: 'https://fsf-p02.herokuapp.com/auth/github/callback'
+  // callbackURL: 'https://fsf-p02.herokuapp.com/auth/github/callback'
+  callbackURL: 'http://127.0.0.1:3000/auth/github/callback'
 }, async (accessToken, refreshToken, profile, cb) => {
   const githubId = profile.id;
   const username = profile.username;
